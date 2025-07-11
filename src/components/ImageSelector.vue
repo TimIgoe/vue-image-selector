0dbba8ad2175d1df26d101041d0b404650f60fd3
Tim Igoe <tim@tsihosting.co.uk>
2025-07-11 23:30:05 +0100

    Move the location of the click event to make it so we can interact with it more than just on the Browse button (so we can hide that if needed)

--- src/components/ImageSelector.vue
+++ src/components/ImageSelector.vue
@@ -68,13 +68,13 @@ const handleBrowse = () => {
                 </svg>
             </div>
             <img v-if="props.state.image" class="image-selector__img" :src="previewUrl()" alt="" @click="handleBrowse">
-            <div v-else class="image-selector__placeholder">
+            <div v-else class="image-selector__placeholder" @click="handleBrowse">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                 </svg>
                 <div class="placeholder-text">
                     <span>Drag & Drop</span><br/>
-                    <span>or <span class="browse-btn" @click="handleBrowse">Browse</span></span><br/>
+                    <span>or <span class="browse-btn">Browse</span></span><br/>
                     <span class="supported-formats">Supports: JPEG, JPG, PNG, WEBP</span>
                 </div>
             </div>
